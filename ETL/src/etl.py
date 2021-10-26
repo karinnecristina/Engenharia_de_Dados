066023321af565b35abf50289acdd98b901858e7
karinnecristina <karinnecristinapereira@gmail.com>
2021-10-26 13:46:02 -0300

    Update repository

--- ETL/src/etl.py
+++ ETL/src/etl.py
@@ -5,48 +5,55 @@ import os
 import pandas as pd
 import numpy as np
 
-pd.set_option('display.max_columns', 20)
+pd.set_option("display.max_columns", 20)
 
 # ==============================================
 # Pastas e subpastas do projeto
 # ==============================================
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(BASE_DIR, 'landing')
+DATA_DIR = os.path.join(BASE_DIR, "landing")
 
 # ==============================================
 # Extração e validação dos dados
 # ==============================================
-def read_csv(filename:str,parse_dates:list):
-    '''Leitura do arquivo'''
-    df = pd.read_csv(os.path.join(DATA_DIR,filename), parse_dates=parse_dates)
+
+
+def read_csv(filename: str, parse_dates: list):
+    """Leitura do arquivo"""
+    df = pd.read_csv(os.path.join(DATA_DIR, filename), parse_dates=parse_dates)
     return df
 
-def validation(df,date:str,id_item:str):
-    '''Validando os tipos de dados'''
+
+def validation(df, date: str, id_item: str):
+    """Validando os tipos de dados"""
     if df.dtypes[[date]].item() == np.object:
-        raise TypeError('O tipo de dado deve ser datetime64[ns].')
+        raise TypeError("O tipo de dado deve ser datetime64[ns].")
     elif df[[id_item]].isnull().values.any():
-        raise TypeError('A coluna não pode conter valores nulos.')
+        raise TypeError("A coluna não pode conter valores nulos.")
     else:
         return df
-    
+
+
 # ==============================================
 # Limpeza dos dados
 # ==============================================
 
-def clean(df,columns:list):
-    '''Padronizando os valores faltantes e removendo as linhas duplicadas'''
-    df[columns] = df[columns].replace(['***','#REF!'], np.nan)
+
+def clean(df, columns: list):
+    """Padronizando os valores faltantes e removendo as linhas duplicadas"""
+    df[columns] = df[columns].replace(["***", "#REF!"], np.nan)
     df = df.drop_duplicates().reset_index(drop=True)
     return df
+
 
 # ==============================================
 # Transformação dos dados
 # ==============================================
 
-def transformation(df,date):
-    '''Criando as colunas de mês, ano e mês_ano'''
-    df['month'] = df[date].dt.month.astype(str)
-    df['year'] = df[date].dt.year.astype(str)
-    df['month_year'] = df['month'] + '-' + df['year']
+
+def transformation(df, date):
+    """Criando as colunas de mês, ano e mês_ano"""
+    df["month"] = df[date].dt.month.astype(str)
+    df["year"] = df[date].dt.year.astype(str)
+    df["month_year"] = df["month"] + "-" + df["year"]
     return df
--- pyspark/src/functions.py
+++ pyspark/src/functions.py
@@ -2,27 +2,25 @@
 #             Bibliotecas
 # =======================================
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,regexp_replace,when,upper,lower
+from pyspark.sql.functions import col, regexp_replace, when, upper, lower
 
 # ==============================================
 # Iniciando o Spark e lendo o conjunto de dados
 # ==============================================
 
 # Define a nossa aplicação Spark
-spark = (SparkSession 
-    .builder 
-    .appName('Pyspark_Commands')
-    .getOrCreate())
-    
+spark = SparkSession.builder.appName("Pyspark_Commands").getOrCreate()
+
 # Leitura do dataframe
-df = (spark.read
-    .format('csv')
-    .option('inferSchema','True')
-    .option('header','True')
-    .load('raw/Employee_Salary_Dataset.csv'))
+df = (
+    spark.read.format("csv")
+    .option("inferSchema", "True")
+    .option("header", "True")
+    .load("raw/Employee_Salary_Dataset.csv")
+)
 
 # =======================================
-#               Funções 
+#               Funções
 # =======================================
 
 # Imprime os datatypes das colunas do dataframe
@@ -38,63 +36,70 @@ df.show(5)
 df.describe()
 
 # Valores únicos de uma determinada coluna
-df.select('Salary').distinct()
+df.select("Salary").distinct()
 
 # Ordenando o dataframe
-df.orderBy(col('Age').desc())
-df.orderBy(col('Experience_Years').asc(),col('Age').desc())
+df.orderBy(col("Age").desc())
+df.orderBy(col("Experience_Years").asc(), col("Age").desc())
 
 # Selecionando colunas
 # Opção 1
-df.select(col('Age'),col('Gender'))
+df.select(col("Age"), col("Gender"))
 
 # Opção 2
-df.select('Age','Gender')
+df.select("Age", "Gender")
 
 # Renomeando uma coluna
-df.withColumnRenamed('Age','Idade')
+df.withColumnRenamed("Age", "Idade")
 
 # Mudando o tipo de uma coluna
-df.withColumn('Salary', col('Salary').cast('double'))
+df.withColumn("Salary", col("Salary").cast("double"))
 
 # Substituindo os valores de uma coluna
-df.withColumn('Gender', regexp_replace(col('Gender'), 'Female', 'F'))
+df.withColumn("Gender", regexp_replace(col("Gender"), "Female", "F"))
 
-df.withColumn('Gender', regexp_replace(col('Gender'), 'Female', 'F')) \
-  .withColumn('Gender', regexp_replace(col('Gender'), 'Male', 'M'))
-  
+df.withColumn("Gender", regexp_replace(col("Gender"), "Female", "F")).withColumn(
+    "Gender", regexp_replace(col("Gender"), "Male", "M")
+)
+
 # Convertendo os valores de uma coluna para maiúsculo e minúsculo
 # Lower (minúsculo)
-df.select(col('Gender'),lower(col('Gender'))).withColumnRenamed('lower(Gender)','Lower')
+df.select(col("Gender"), lower(col("Gender"))).withColumnRenamed(
+    "lower(Gender)", "Lower"
+)
 
 # Upper (maiúsculo)
-df.select(col('Gender'),upper(col('Gender'))).withColumnRenamed('upper(Gender)','Upper')
-  
+df.select(col("Gender"), upper(col("Gender"))).withColumnRenamed(
+    "upper(Gender)", "Upper"
+)
+
 # Criando novas colunas
-df = df.withColumn('Age_Group',
-        when(col('Age') < 19,'Young') \
-       .when((col('Age') >= 20) & (col('Age') <= 59),'Adult') \
-       .when(col('Age') >= 60, 'Old'))
-   
+df = df.withColumn(
+    "Age_Group",
+    when(col("Age") < 19, "Young")
+    .when((col("Age") >= 20) & (col("Age") <= 59), "Adult")
+    .when(col("Age") >= 60, "Old"),
+)
+
 # Removendo uma ou mais colunas
-df.drop('Experience_Years')
-df.drop('Experience_Years','ID')
+df.drop("Experience_Years")
+df.drop("Experience_Years", "ID")
 
 # Filtrando valores
 # Filter
 df.filter(df.Salary > 40000)
-df.filter((df.Salary > 40000) & (df.Gender == 'Female'))
+df.filter((df.Salary > 40000) & (df.Gender == "Female"))
 
 # Where
 df.where(df.Salary > 40000)
-df.where((df.Salary > 40000) & (df.Gender == 'Female'))
+df.where((df.Salary > 40000) & (df.Gender == "Female"))
 
 # Select e Where
-df.select(df.Age,df.Gender,df.Salary).where(df.Salary > 400000)
+df.select(df.Age, df.Gender, df.Salary).where(df.Salary > 400000)
 
 # GroupBy
 # Somatória do salário por sexo
-df.groupBy('Gender').sum('Salary').withColumnRenamed('sum(Salary)','Sum')
+df.groupBy("Gender").sum("Salary").withColumnRenamed("sum(Salary)", "Sum")
 
 # Média de salário por faixa etária
-df.groupBy('Age_Group').mean('Salary').withColumnRenamed('mean(Salary)','Mean')
+df.groupBy("Age_Group").mean("Salary").withColumnRenamed("mean(Salary)", "Mean")
