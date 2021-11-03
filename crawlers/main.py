d382d485721ff5be80a1737e2b35734fb04f9cd2
karinnecristina <karinnecristinapereira@gmail.com>
2021-11-03 20:56:42 -0300

    Collect information from real estate funds

--- crawlers/main.py
+++ crawlers/main.py
@@ -1,7 +1,9 @@
 # ==============================================
 #                 Libraries
 # ==============================================
+import time
 import warnings
+from datetime import datetime
 from selenium import webdriver
 
 warnings.filterwarnings("ignore")
@@ -19,6 +21,8 @@ elem_funds = driver.find_element_by_xpath(
     '//*[@id="quick-access"]/div[2]/div[1]/div[1]'
 ).click()
 
+time.sleep(5)
+
 # ==============================================
 #          Clearing the search bar
 # ==============================================
@@ -35,12 +39,44 @@ elem_funds.send_keys("HGLG11")
 # ==============================================
 elem_cmb = driver.find_element_by_xpath('//*[@id="item-HGLG11"]/a/span').click()
 
+data = datetime.today().strftime("%d-%m-%Y")
+
 price = driver.find_element_by_xpath(
     "/html/body/section/section/div/div/div/div[3]/div/span[1]"
 ).text
+
+variation = driver.find_element_by_xpath(
+    "/html/body/section/section/div/div/div/div[3]/div/span[2]"
+).text
+
 liquidez = driver.find_element_by_xpath(
     "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[1]/span[2]"
 ).text
+
+last_income = driver.find_element_by_xpath(
+    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[2]/span[2]"
+).text
+
+dividend_yield = driver.find_element_by_xpath(
+    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[3]/span[2]"
+).text
+
+patrimony = driver.find_element_by_xpath(
+    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[4]/span[2]"
+).text
+
+equity_value = driver.find_element_by_xpath(
+    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[5]/span[2]"
+).text
+
+profitability_month = driver.find_element_by_xpath(
+    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[6]/span[2]"
+).text
+
+p_vp = driver.find_element_by_xpath(
+    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[7]/span[2]"
+).text
+
 
 # ==============================================
 #     Closing the connection to the website
@@ -49,8 +85,25 @@ driver.close()
 
 print(
     """
-preço: {}
-liquidez: {}""".format(
-        price, liquidez
+Data: {}
+Preco: {}
+Liquidez: {}
+Ultimo_Rendimento: {}
+Variacao: {}
+Dividend_Yield: {}
+Patrimonio_Liquido: {}
+Valor_Patrimonial: {}
+Rentabilidade_mes: {}
+P/VP: {}""".format(
+        data,
+        price,
+        liquidez,
+        last_income,
+        variation,
+        dividend_yield,
+        patrimony,
+        equity_value,
+        profitability_month,
+        p_vp,
     )
 )
