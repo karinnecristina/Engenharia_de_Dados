eb7a09b601480af11f51d0aebb85bb73ce990b6f
karinnecristina <karinnecristinapereira@gmail.com>
2021-11-05 21:08:21 -0300

    Create dataframe

--- crawlers/main.py
+++ crawlers/main.py
@@ -1,6 +1,7 @@
 # ==============================================
 #                 Libraries
 # ==============================================
+import pandas as pd
 import time
 import warnings
 from datetime import datetime
@@ -24,86 +25,92 @@ elem_funds = driver.find_element_by_xpath(
 time.sleep(5)
 
 # ==============================================
-#          Clearing the search bar
-# ==============================================
-elem_funds = driver.find_element_by_name("fii")
-elem_funds.clear()
-
-# ==============================================
-#        Searching a real estate fund
-# ==============================================
-elem_funds.send_keys("HGLG11")
-
-# ==============================================
 #    Extracting real estate fund information
 # ==============================================
-elem_cmb = driver.find_element_by_xpath('//*[@id="item-HGLG11"]/a/span').click()
 
-data = datetime.today().strftime("%d-%m-%Y")
+funds = ["HGLG11", "XPLG11", "KNRI11", "HGRU11", "HGRE11"]
+data = []
 
-price = driver.find_element_by_xpath(
-    "/html/body/section/section/div/div/div/div[3]/div/span[1]"
-).text
+for element in funds:
+    elem_funds = driver.find_element_by_name("fii")
+    elem_funds.clear()
+    elem_funds.send_keys(element)
+    elem_cmb = driver.find_element_by_xpath(f'//*[@id="item-{element}"]/a/span').click()
 
-variation = driver.find_element_by_xpath(
-    "/html/body/section/section/div/div/div/div[3]/div/span[2]"
-).text
+    date = datetime.today().strftime("%d-%m-%Y %H:%M")
 
-liquidez = driver.find_element_by_xpath(
-    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[1]/span[2]"
-).text
+    price = driver.find_element_by_xpath(
+        "/html/body/section/section/div/div/div/div[3]/div/span[1]"
+    ).text
 
-last_income = driver.find_element_by_xpath(
-    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[2]/span[2]"
-).text
+    variation = driver.find_element_by_xpath(
+        "/html/body/section/section/div/div/div/div[3]/div/span[2]"
+    ).text
 
-dividend_yield = driver.find_element_by_xpath(
-    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[3]/span[2]"
-).text
+    liquidez = driver.find_element_by_xpath(
+        "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[1]/span[2]"
+    ).text
 
-patrimony = driver.find_element_by_xpath(
-    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[4]/span[2]"
-).text
+    last_income = driver.find_element_by_xpath(
+        "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[2]/span[2]"
+    ).text
 
-equity_value = driver.find_element_by_xpath(
-    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[5]/span[2]"
-).text
+    dividend_yield = driver.find_element_by_xpath(
+        "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[3]/span[2]"
+    ).text
 
-profitability_month = driver.find_element_by_xpath(
-    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[6]/span[2]"
-).text
+    patrimony = driver.find_element_by_xpath(
+        "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[4]/span[2]"
+    ).text
 
-p_vp = driver.find_element_by_xpath(
-    "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[7]/span[2]"
-).text
+    equity_value = driver.find_element_by_xpath(
+        "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[5]/span[2]"
+    ).text
 
+    profitability_month = driver.find_element_by_xpath(
+        "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[6]/span[2]"
+    ).text
 
-# ==============================================
-#     Closing the connection to the website
-# ==============================================
+    p_vp = driver.find_element_by_xpath(
+        "/html/body/section/div[1]/section[1]/div/div/div/div/div/div[7]/span[2]"
+    ).text
+
+    data.append(
+        (
+            date,
+            price,
+            liquidez,
+            last_income,
+            variation,
+            dividend_yield,
+            patrimony,
+            equity_value,
+            profitability_month,
+            p_vp,
+        )
+    )
+
+    driver.execute_script("window.history.go(-1)")
+    time.sleep(2)
+
 driver.close()
 
-print(
-    """
-Data: {}
-Preco: {}
-Liquidez: {}
-Ultimo_Rendimento: {}
-Variacao: {}
-Dividend_Yield: {}
-Patrimonio_Liquido: {}
-Valor_Patrimonial: {}
-Rentabilidade_mes: {}
-P/VP: {}""".format(
-        data,
-        price,
-        liquidez,
-        last_income,
-        variation,
-        dividend_yield,
-        patrimony,
-        equity_value,
-        profitability_month,
-        p_vp,
-    )
+# ==============================================
+#    DataFrame with collected information
+# ==============================================
+
+df = pd.DataFrame(
+    data,
+    columns=[
+        "Data",
+        "Preco",
+        "Liquidez",
+        "Ultimo_Rendimento",
+        "Variacao",
+        "Dividend_Yield",
+        "Patrimonio_Liquido",
+        "Valor_Patrimonial",
+        "Rentabilidade_mes",
+        "P/VP",
+    ],
 )
