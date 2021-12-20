d04be391771395c41f775222aac085fefa626e85
karinnecristina <karinnecristinapereira@gmail.com>
2021-12-20 16:13:55 -0300

    Update airflow dag script

--- Projeto/dags/dag_funds.py
+++ Projeto/dags/dag_funds.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
-from airflow.operators.email_operator import EmailOperator
 from fii_dag.collect_fii import FundsExplorer
 
 collect_fii = FundsExplorer(
@@ -18,12 +17,8 @@ collect_fii = FundsExplorer(
     ]
 )
 
-
 args = {
     "owner": "airflow",
-    "depends_on_past": False,
-    "email_on_failure": True,
-    "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
@@ -31,8 +26,9 @@ args = {
 dag = DAG(
     dag_id="fundos_imobiliarios",
     default_args=args,
-    schedule_interval="38 23 * * *",
+    schedule_interval="31 18 * * *",
     start_date=datetime(2021, 12, 17),
+    catchup=False,
 )
 
 extract_task = PythonOperator(
@@ -52,17 +48,9 @@ bucket_task = PythonOperator(
 load_task = PythonOperator(
     task_id="carregar_dados_s3",
     email_on_failure=True,
-    email="teste@gmail.com",
     python_callable=collect_fii.send_files_s3,
     do_xcom_push=False,
     dag=dag,
 )
 
-email_task = EmailOperator(
-    task_id="enviar_email",
-    to="teste@gmail.com",
-    subject="Pipeline finalizado",
-    html_content="<p> Os dados forma carregados no bucket com sucesso! <p>",
-)
-
-extract_task >> bucket_task >> load_task >> email_task
+extract_task >> bucket_task >> load_task
